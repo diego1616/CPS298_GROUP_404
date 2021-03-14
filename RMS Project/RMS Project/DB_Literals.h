ee9d090ea3817afa93ba89b55e54b66cc0d63f3b
Dan Santos <drsantos@wccnet.edu>
2021-03-13 20:00:21 -0500

    DB Literals

--- RMS Project/RMS Project/DB_Literals.h
+++ RMS Project/RMS Project/DB_Literals.h
@@ -1,6 +1,87 @@
 #pragma once
-class DB_Literals
-{
-};
 
+#include <string>
 
+// Note: const implies static in this case
+namespace product_table {
+	const string product_id = "product_id";
+  	const string item_name = "item_name";
+	const string manufacturer_id = "manufacturer_id";
+	const string manufacturer_product_id = "manufacturer_product_id";
+	const string manufacturer_price = "manufacturer_price";
+	const string retail_price = "retail_price";
+	const string department_id = "department_id";
+	const string low_stock_quantity = "low_stock_quantity";
+}
+
+namespace manufacturer_table {
+	const string manufacturer_id = "manufacturer_id";
+	const string manufacturer_name = "manufacturer_name";
+	const string contact_name = "contact_name";
+	const string contact_email = "contact_email";
+	const string street_address = "street_address";
+	const string street_name = "street_name";
+	const string city = "city";
+	const string zip_code = "zip_code";
+	const string state = "state";
+	const string country = "country";
+	const string phone_number = "phone_number";
+}
+
+namespace department_table {
+	const string department_id = "department_id";
+	const string department_name = "department_name";
+}
+
+namespace inventory_amounts_table {
+	const string inventory_id = "inventory_id";
+	const string product_id = "product_id";
+	const string product_count = "product_count";
+}
+
+namespace keyword_table {
+	const string keyword_id = "keyword_id";
+	const string keyword_name = "keyword_name";
+}
+
+namespace product_keyword_table {
+	const string p_key_id = "p_key_id";
+	const string keyword_id = "keyword_id";
+	const string product_id = "product_id";
+}
+
+namespace storage_table {
+	const string storage_id = "storage_id";
+	const string storage_name = "storage_name";
+}
+
+namespace storage_location_table {
+	const string storage_loc_id = "storage_loc_id";
+	const string storage_id = "storage_id";
+	const string product_id = "product_id";
+}
+
+namespace sales_location_table {
+	const string sales_loc_id = "sales_loc_id";
+	const string sales_loc_name = "sales_loc_name";
+}
+
+namespace product_sales_location_table {
+	const string product_sales_loc_id = "product_sales_loc_id";
+	const string sales_loc_id = "sales_loc_id";
+	const string product_id = "product_id";
+}
+
+namespace user_table {
+	const string user_id = "user_id";
+	const string f_name = "f_name";
+	const string l_name = "l_name";
+	const string job_id = "job_id";
+	const string department_id = "department_id";
+	const string password = "password";
+}
+
+namespace job_table {
+	const string job_id = "job_id";
+	const string job_name = "job_name";
+}
