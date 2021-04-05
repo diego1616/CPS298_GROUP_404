c2ddfda7a4762894084b70aa925d858a116e137f
terina00 <60524595+terina00@users.noreply.github.com>
2021-04-05 16:49:21 -0400

    Create MainMenu.h

---
+++ RMS/MainMenu.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "C:\CPS298_GROUP_404\CPS298_GROUP_404\RMS\DB_Connect.h"
+
+
+using namespace std;
+
+
+class MainMenu
+{
+private:
+
+public:
+	void print_main_menu();
+	void menu_options();
+};
